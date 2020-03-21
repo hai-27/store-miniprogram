2e8d7e8eb3db04ecac0adc439888bf22d823f134
hai-27 <2601615158@qq.com>
2020-03-21 22:08:55 +0800

    Add product list.

--- components/weui/SearchBar/searchbar.js
+++ components/weui/SearchBar/searchbar.js
@@ -1,27 +1,27 @@
 Page({
-    data: {
-        inputShowed: false,
-        inputVal: ""
-    },
-    showInput: function () {
-        this.setData({
-            inputShowed: true
-        });
-    },
-    hideInput: function () {
-        this.setData({
-            inputVal: "",
-            inputShowed: false
-        });
-    },
-    clearInput: function () {
-        this.setData({
-            inputVal: ""
-        });
-    },
-    inputTyping: function (e) {
-        this.setData({
-            inputVal: e.detail.value
-        });
-    }
+  data: {
+    inputShowed: false,
+    inputVal: ""
+  },
+  showInput: function() {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function() {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function() {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function(e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  }
 });
--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -1,66 +1,131 @@
 // pages/goods/goods.js
+const {
+  baseURL,
+  $ajax
+} = getApp().globalData;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    currentPage: 1,
+    pageSize: 15,
+    current_scroll: 0,
+    category: [],
+    search: '',
+    product: [],
+    productTotal: 0
+  },
+  // 选择分类
+  handleChangeScroll({
+    detail
+  }) {
+    this.setData({
+      search: '',
+      current_scroll: detail.key,
+      currentPage: 1,
+      product: []
+    });
+    this.getData(); // 获取商品数据
+  },
+  // 获取商品数据
+  async getData() {
+    let api = '';
+    if (this.data.search) {
+      api = 'product/getProductBySearch';
+    } else {
+      api = this.data.current_scroll == 0 ? 'product/getAllProduct' : 'product/getProductByCategory';
+    }
+
+    let productTemp = await $ajax(api, {
+      data: {
+        search: this.data.search,
+        categoryID: this.data.current_scroll,
+        currentPage: this.data.currentPage,
+        pageSize: this.data.pageSize
+      }
+    });
+    console.log(productTemp);
+    const product = this.data.product.concat(productTemp.Product);
+    this.setData({
+      product,
+      productTotal: productTemp.total
+    })
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: async function(options) {
+    // 从后端获取需要的数据
+    this.getData(); // 获取商品数据
+    let categoryRes = await $ajax('product/getCategory'); // 分类列表
 
+    // 处理分类列表，添加“全部”
+    const all = {
+      category_id: 0,
+      category_name: "全部"
+    };
+    let categoryTemp = categoryRes.category
+    categoryTemp.unshift(all);
+
+    this.setData({
+      category: categoryTemp
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
+  onReachBottom: function() {
+    if (this.data.productTotal > this.data.product.length) {
+      this.setData({
+        currentPage: this.data.currentPage + 1
+      })
+      this.getData();
+    }
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
