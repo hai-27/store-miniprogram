91e8bb90fa4454014efdfb41aa7d3daf7cd2822e
hai-27 <2601615158@qq.com>
2020-03-22 22:34:10 +0800

    Add product details page.

---
+++ pages/details/details.js
@@ -0,0 +1,82 @@
+// pages/details/details.js
+const {
+  baseURL,
+  $ajax
+} = getApp().globalData;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    baseURL,
+    productID: '',
+    productImages: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: async function(options) {
+    const {productID} = options; // 接收路由传递的商品id
+    // 从后端获取需要的数据
+    let productImages = await $ajax('product/getDetailsPicture', {
+      data: {
+        productID
+      }
+    })
+    this.setData({
+      productID,
+      productImages: productImages.ProductPicture
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
